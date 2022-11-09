4feaac12ea04bd877651eb126f4ec804385b70f9
OrangerLai <117832941+OrangerLai@users.noreply.github.com>
2022-11-09 22:38:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -716,16 +716,5 @@ S.Shape = (function () {
 
 S.init();
 
-function audioAutoPlay (id) {
-var audio=document.getElementById(id);
-audio.play();
-document.addEventListener("WeixinJSBridgeReady",function() {
-audio.play();
-},false);
-document.addEventListener("YixinJSBridgeReady",function() {
-audio.play();
-},false);
-}
-audioAutoPlay("myaudio");
 
 
